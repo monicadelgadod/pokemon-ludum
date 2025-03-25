ce655bb2b318e1de8f40af43eaf06a79fd9dd096
Monica Delgado <monicadelgadod11@gmail.com>
2025-03-25 14:02:22 +0100

    Pantalla del juego de Pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Hola Mundo</h1>
+    <PokemonLudum/>
   </div>
 </template>
 
 <script lang="ts" setup>
+import PokemonLudum from './modulorum/pokemon/paginae/PokemonLudum.vue';
 
+  
 </script>
 
 <style lang="scss" scoped>
---
+++ src/modulorum/pokemon/paginae/PokemonLudum.vue
@@ -0,0 +1,25 @@
+<template>
+    <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="text-3xl">Espere, por favor</h1>
+        <h3 class="animate-pulse">Cargando Pokemons</h3>
+    </section>
+
+    <section class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="m-5">¿Quién es este Pokemon?</h1>
+
+        <Pokemonimago/>
+        <PokemonOptiones/>
+
+    </section>
+</template>
+
+<script setup lang="ts">
+import Pokemonimago from '../components/PokemonImago.vue';
+import PokemonOptiones from '../components/PokemonOptiones.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>

ecf980b3679cb35d5d6eb567423a6aa6fe29f2fe
Monica Delgado <monicadelgadod11@gmail.com>
2025-04-01 16:59:18 +0200

    Pokemon al azar y preparada la visibilidad

--- src/modulorum/pokemon/composables/usePokemonLudum.ts
+++ src/modulorum/pokemon/composables/usePokemonLudum.ts
@@ -1,19 +1,25 @@
 import { computed, onMounted, ref } from "vue";
 import { LudumStatus, type Pokemon, type PokemonListaResponsio } from "../interfaces";
 import { pokemonApi } from "@/api/pokemonApi";
+
 export const usePokemonLudum = () => {
 
     const ludumStatus = ref< LudumStatus>(LudumStatus.Ludit);
     const  pokemons = ref<Pokemon[]>([]);
-
     const pokemonOptiones = ref<Pokemon[]>([]);
 
-    const estPortat = computed(() => pokemons.value.length === 0);
+    const estPortat = computed( () => pokemons.value.length === 0);
+
+    const temerePokemon = computed( () => {
+        const temereIndex = Math.floor(Math.random() * pokemonOptiones.value.length);
+        return pokemonOptiones.value[temereIndex];
+
+    });
 
 
     const obtinePokemons = async(): Promise<Pokemon[]> => {
         const responsio = await pokemonApi.get<PokemonListaResponsio>('/?limit=151');
-        const pokemonArray: Pokemon[] = responsio.data.results.map(() =>{
+        const pokemonArray: Pokemon[] = responsio.data.results.map((pokemon) =>{
 
             const urlPartes = pokemon.url.split('/');
             const id = urlPartes[urlPartes.length - 2] ?? 0;
@@ -46,9 +52,7 @@ const sequentiOptiones = (quot: number = 4) => {
         estPortat,
         pokemonOptiones,
         sequentiOptiones,
+        temerePokemon,
     }
 }
 
-function computed() {
-    throw new Error("Function not implemented.");
-}
--- src/modulorum/pokemon/paginae/PokemonLudum.vue
+++ src/modulorum/pokemon/paginae/PokemonLudum.vue
@@ -1,24 +1,27 @@
 <template>
-    <section v-if="estPortat" class="flex flex-col justify-center items-center w-screen h-screen">
+    <section v-if="estPortat || temerePokemon.id === null" class="flex flex-col justify-center items-center w-screen h-screen">
         <h1 class="text-3xl">Espere, por favor</h1>
         <h3 class="animate-pulse">Cargando Pokemons</h3>
     </section>
 
     <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
         <h1 class="m-5">¿Quién es este Pokemon?</h1>
+        <h3>{{ temerePokemon }}</h3>
 
-        <Pokemonimago/>
+        <PokemonImago :pokemon-id="temerePokemon.id" :revelare-pokemon="ludumStatus !== ludumStatus.Ludit" />
         <PokemonOptiones/>
 
     </section>
 </template>
 
 <script setup lang="ts">
-import Pokemonimago from '../components/PokemonImago.vue';
+import PokemonImago from '../components/PokemonImago.vue';
 import PokemonOptiones from '../components/PokemonOptiones.vue';
 import { usePokemonLudum } from '../composables/usePokemonLudum';
 
-const {estPortat , ludumStatus} = usePokemonLudum();
+const {ludumStatus, estPortat, temerePokemon} = usePokemonLudum();
+
+
 
 
 </script>
